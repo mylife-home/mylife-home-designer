27f98e37a700d062262a071b6f570f6dde1e9ff9
Vincent Trumpff <vincent@stoic.com>
2015-11-29 17:33:13 +0100

    client init

---
+++ public/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import 'angular';
+import 'angular-route';
+import 'angular-bootstrap';
+//import './components/image.js';
+//import './views/window.js';
+
+angular.module('mylife-home-designer', [
+  'ngRoute',
+  'ui.bootstrap'
+//  'mylife-home-ui.components.data'
+]);
