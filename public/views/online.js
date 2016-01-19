2512a8677b630503c586c56bca69ff8ec04e1390
Vincent Trumpff <vincent@stoic.com>
2016-01-19 08:57:00 +0100

    style

--- public/views/online.js
+++ public/views/online.js
@@ -5,4 +5,5 @@ angular.module('mylife-home-designer.views')
 .controller('onlineController', function (repository, images) {
 
   this.entities = repository.entities;
+  this.types    = repository.types;
 });
