c3f3f57c79972afc1ffbf0609712cca38b086be9
Vincent Trumpff <vincent@stoic.com>
2016-01-29 14:42:17 +0100

    layout

--- public/views/online.js
+++ public/views/online.js
@@ -13,9 +13,45 @@ angular.module('mylife-home-designer.views')
     this.detail = null;
   }
 
-  this.showEntityDetail    = function(entity)            { this.detail = { type: 'entity',    entity }; };
-  this.showResourceDetail  = function(entity, key)       { this.detail = { type: 'resource',  entity, key }; };
-  this.showPluginDetail    = function(entity, plugin)    { this.detail = { type: 'plugin',    entity, plugin }; };
-  this.showComponentDetail = function(entity, component) { this.detail = { type: 'component', entity, component }; };
+  this.showEntityDetail = function(entity) {
+    this.detail = {
+      type: 'entity',
+      entity
+    };
+  };
+
+  this.showResourceDetail = function(entity, key) {
+    this.detail = {
+      type: 'resource',
+      entity,
+      key
+    };
+  };
+
+  this.showPluginDetail = function(entity, plugin) {
+    this.detail = {
+      type: 'plugin',
+      entity,
+      plugin
+    };
+  };
+
+  this.showComponentDetail = function(entity, component) {
+    this.detail = {
+      type: 'component',
+      entity,
+      component,
+      plugin: findComponentPlugin(entity, component)
+    };
+  };
+
+  function findComponentPlugin(entity, component) {
+    for(let plugin of entity.plugins) {
+      if(plugin.library === component.library &&
+         plugin.type === component.type) {
+        return plugin;
+      }
+    }
+  }
 
 });
